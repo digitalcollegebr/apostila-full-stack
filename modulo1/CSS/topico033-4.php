deee2fcce061fd89b9a252f4e4bed0df00bf5858
Jennifer Anne <jennifer@DIGITALCOLLEGE.LOCAL>
2024-09-23 15:31:35 -0300

    finalização do topico033-4 CSS

---
+++ modulo1/CSS/topico033-4.php
@@ -0,0 +1,30 @@
+<?php
+    // A_S - Header
+    include('../../layouts/header.php');
+?>
+
+<body>
+    <!-- A_S - Habilita o VLibras -->
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    </script>    
+
+
+    <div class="container" style="margin-top: 100px">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-lg-12">
+            </div>
+        </div>
+    </div>
+    <a href="../../questoesModulo01/unidade06.php" class="button-prox-atv"><img src="../../assets/img/arrow-circle-right (2).png" alt="" class="next"></a>
+
+    <?php include('../../layouts/footer.php') ?>
+</body>
+</html>
